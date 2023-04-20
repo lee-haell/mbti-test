dca07efb98e82b74bb8b5ff802eac7b812044edf
Rin <64449865+lee-haell@users.noreply.github.com>
2023-04-20 20:57:24 +0900

    edit-답변 영역 js 수정

--- assets/js/question.js
+++ assets/js/question.js
@@ -2,17 +2,27 @@ const main = document.querySelector('#main');
 const qna = document.querySelector('#qna');
 qna.style.display = 'none';
 
-function addAnswer(){
+function addAnswer(answerText, qIndex){
     var a = document.querySelector('.answerBox');
     var answer = document.createElement('button');
+    answer.classList.add('answerList');
     a.appendChild(answer);
+    answer.innerHTML = answerText;
+    answer.addEventListener('click', function(){
+        var children = document.querySelectorAll('.answerList');
+        for(let i = 0; i < children.length; i++){ //답변 클릭 시 답변들 모두 비활성화
+            children[i].disabled = true;
+            children[i].style.display = 'none';
+        }
+        goNext(++qIndex);
+    }, false);
 }
 
 function goNext(qIndex){
     var q = document.querySelector('.questionBox');
     q.innerHTML = qnaList[qIndex].q;
     for(let i in qnaList[qIndex].a){ //반복 횟수
-        addAnswer(qnaList[qIndex].a[qIndex].answer); //무엇을 반복? 답변버튼
+        addAnswer(qnaList[qIndex].a[i].answer, qIndex); //무엇을 반복? 답변버튼
     }
 }
 
